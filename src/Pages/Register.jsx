0aa1ca0bbe0f98e31e25a7c3f875c66b84689c04
Khabbab Bin Bashar <binbashark@gmail.com>
2025-11-18 22:55:22 +0600

    profile update functionality added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -39,7 +39,6 @@ const Register = () => {
 
     return (
         <div className="hero bg-base-200 min-h-screen">
-
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-5">
                     <h1 className="text-5xl font-bold">Register now!</h1>
